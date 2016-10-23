75eaee31eff532eced33a757b839c715c88a4da1
Student <Student>
2016-10-23 10:51:12 -0500

    My first commit, able to create book and set a title to it.

---
+++ FirstAssignment/BookList.cpp
@@ -0,0 +1,27 @@
+#include "BookList.h"
+#include "Book.h"
+
+
+BookList::BookList() {
+
+}
+
+BookList::~BookList() {
+
+}
+
+void BookList::search(string t) {
+
+}
+
+void BookList::insert(string t, int q, double p) {
+
+}
+
+void BookList::remove(string t) {
+
+}
+
+void BookList::print() {
+
+}
---
+++ FirstAssignment/Main.cpp
@@ -0,0 +1,13 @@
+#include "Book.h"
+#include "BookList.h"
+
+int main() {
+
+	Book myBook;
+	myBook.title = "Fire";
+	
+
+	cout << "Hello World" << endl;
+
+	system("pause");
+}
